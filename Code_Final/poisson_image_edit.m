41a83965ba68ddf9b6f8073c36a58463e9f23d68
Siddharth Shrivastava <siddharthmanik16@gmail.com>
2021-04-12 12:47:19 +0530

    Update poisson_image_edit.m

--- Code_Final/poisson_image_edit.m
+++ Code_Final/poisson_image_edit.m
@@ -19,34 +19,6 @@ for i = 1:x
     end 
 end 
 
-% subject(mask(:)) 
-% subject(mask==1) = example(mask==1);
-
 result = PoissonGaussSeidel(subject, s_gx, s_gy, mask);
-
-
-% [x, y] = size(subject);
-% result = subject; 
-% 
-% 
-% % h = [0 -1 0; -1 4 -1; 0 -1 0];
-% h = [[0, 0, -1, 0, 0], [0, -1, -2, -1, 0], [-1, -2, 16, -2, -1], [0, -1, -2, -1, 0], [0, 0, -1, 0, 0]]; 
-% 
-% LaplacianTarget = imfilter(double(subject), h, 'replicate');
-% LaplacianSource = imfilter(double(example), h, 'replicate');
-% grad_res = LaplacianTarget;
-% 
-% sigma = 2;
-% guassianTarget = imgaussfilt(subject,sigma);
-% 
-% for i = 1:x 
-%     for j = 1:y 
-%         if abs(LaplacianSource(i,j))*beta(i,j) > abs(LaplacianTarget(i,j))
-%             grad_res(i,j) = LaplacianSource(i,j);
-%         end 
-%     end 
-% end 
-% 
-% result = grad_res + guassianTarget;
 
 end 
